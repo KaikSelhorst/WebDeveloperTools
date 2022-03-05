256e43329262bc039adaafc3c2a504b18b690d8b
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2022-03-05 17:31:05 -0400

    Add all Files

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const moonDark = document.getElementById("moon");
+
+
+ moonDark.addEventListener('click',(event) =>{
+   document.body.classList.toggle('darkmode')
+ })
+ 
+const sunLight = document.getElementById("sun");
+
+sunLight.addEventListener('click',(event) =>{
+  document.body.classList.toggle('darkmode')
+})
