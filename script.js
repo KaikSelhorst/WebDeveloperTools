0cfe85b984ed13a8eda1dd2546da6546b9287613
Kaik Selhorst <82120356+KaikSelhorst@users.noreply.github.com>
2022-03-11 10:40:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 
+// Defaut Theme
+
+if (localStorage.theme == 'darkmode') {
+  document.body.classList.add('darkmode')
+}
+
+// Dark Theme
 const moonDark = document.getElementById("moon");
 
+moonDark.addEventListener('click', (event) => {
+  document.body.classList.toggle('darkmode')
+  localStorage.setItem('theme', 'darkmode')
+})
 
- moonDark.addEventListener('click',(event) =>{
-   document.body.classList.toggle('darkmode')
- })
- 
+// White Theme
 const sunLight = document.getElementById("sun");
 
-sunLight.addEventListener('click',(event) =>{
+sunLight.addEventListener('click', (event) => {
   document.body.classList.toggle('darkmode')
+  localStorage.setItem('theme', 'whiteMode')
 })
 
